a6d7a2ed18c05cc28f56e56e172c34e1ec38ab57
Hrushi01 <hrushikesh.ambike@gmail.com>
2022-09-13 13:18:46 +0530

    Deleted

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { useCreatePostMutation } from "../services/post";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import { useCreatePostMutation } from "../services/post";
+// import { Link } from "react-router-dom";
 
-function Create() {
-  const [createPost, responceinfo] = useCreatePostMutation();
+// function Create() {
+//   const [createPost, responceinfo] = useCreatePostMutation();
 
-  const newpost = {
-    name: "morpheus",
-    job: "leader",
-  };
+//   const newpost = {
+//     name: "morpheus",
+//     job: "leader",
+//   };
 
-  return (
-    <div>
-      Create Data
-      <form action=""></form>
-      <Link to={`add-user`}>
-        <button className="bg-slate-500">Add user</button>
-      </Link>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       Create Data
+//       <form action=""></form>
+//       <Link to={`add-user`}>
+//         <button className="bg-slate-500">Add user</button>
+//       </Link>
+//     </div>
+//   );
+// }
 
-export default Create;
+// export default Create;
